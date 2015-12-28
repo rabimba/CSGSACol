b2cb1e2d9e22588a89688d34447c237ddd249667
Rabimba Karanjai <rabimba@yahoo.com>
2015-12-28 05:05:07 -0600

    Try 1 changing zoom

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,7 @@ $(function(){
 	
 	var timeline = new VMM.Timeline();
 	timeline.init("data.json");
-
+ var timeline_config = {
+            start_zoom_adjust:  '10',                            //OPTIONAL TWEAK THE DEFAULT ZOOM LEVEL
+        }
 });
