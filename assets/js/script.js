1a31ed4e67687e0174d23568309518cfd8443df0
Rabimba Karanjai <rabimba@yahoo.com>
2016-01-03 00:51:35 -0600

    Change to createstory

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
 $(function(){
 	
-	var timeline = new VMM.Timeline();
-	timeline.init("data.json");
- var timeline_config = {
-            start_zoom_adjust:  '10',                            //OPTIONAL TWEAK THE DEFAULT ZOOM LEVEL
+// 	var timeline = new VMM.Timeline();
+// 	timeline.init("data.json");
+//  var timeline_config = {
+//             start_zoom_adjust:  '10',                            //OPTIONAL TWEAK THE DEFAULT ZOOM LEVEL
+$(document).ready(function() {
+                createStoryJS({
+                    type:       'timeline',
+                    source:     'data.json',
+                    embed_id:   'timeline',
+					start_zoom_adjust:	'10',
+					start_at_end:	'true'
+                });
+            });
         }
 });
