f14d0c8f28543ceae08b628cc2dbb607cf75f008
Rabimba Karanjai <rabimba@yahoo.com>
2016-01-08 03:10:44 -0600

    Old code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,6 @@
 $(function(){
 	
-// 	var timeline = new VMM.Timeline();
-// 	timeline.init("data.json");
-//  var timeline_config = {
-//             start_zoom_adjust:  '10',                            //OPTIONAL TWEAK THE DEFAULT ZOOM LEVEL
-$(document).ready(function() {
-                createStoryJS({
-                    type:       'timeline',
-                    source:     'data.json',
-                    embed_id:   'timeline',
-					start_zoom_adjust:	'10',
-					start_at_end:	'true'
-                });
-            });
-        // }
+	var timeline = new VMM.Timeline();
+	timeline.init("data.json");
+
 });
